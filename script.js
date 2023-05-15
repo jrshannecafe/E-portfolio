b1da2a758af36967255805744d952dadbe18dd92
jrshannecafe <131421226+jrshannecafe@users.noreply.github.com>
2023-05-15 06:59:08 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+/* ============================= Landing Page ============================= */
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function() {
+    header.classList.toggle ("sticky", window.scrollY > 100);
+
+})
+/* ============================= Education Tabs =============================*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active') 
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
+document.querySelector('.button input[type="button"]').addEventListener('click', function() {
+    alert('Your message has been submitted! Thank you for contacting me.');
+});
